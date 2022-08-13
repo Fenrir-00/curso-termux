ecea6d9929dd53d42e5293a810cdc41bf8eeebf3
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-13 15:46:42 +0200

    Update curso.py

--- curso.py
+++ curso.py
@@ -116,8 +116,8 @@ def menu():
 [3]LIMPIAR CONSOLA               CLEAR
 [4]PAQUETES                      PKG
 [5]HERRQMIENTAS                  PT
-[6]BORRA ARCHIVOS                RM                                                       
-[7]ENTRAR EN UNA CARPETA         CD                                       
+[6]BORRA ARCHIVOS                RM                                                
+[7]ENTRAR EN UNA CARPETA         CD
 [8]COPIAR UN ARCHIVO             CP
 [9]CREAR UN ARCHIVO              TOUCH
 [10]LEER UN ARCHIVO              CAT
