3b67ffe744952dfce5e7895591ea4dd27e5c9ec4
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-13 15:34:38 +0200

    Update curso.py

--- curso.py
+++ curso.py
@@ -59,6 +59,32 @@ def banner():
 ██║     ███████╗██║ ╚███║██║  ██║██║██║  ██║
 ╚═╝     ╚══════╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝""")
  print(f"{color.fin}")
+def incorrecto():
+    os.system("clear")
+    print(f"""{color.rojo}
+░█████╗░██████╗░░█████╗░██╗░█████╗░███╗░░██╗
+██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗░██║
+██║░░██║██████╔╝██║░░╚═╝██║██║░░██║██╔██╗██║
+██║░░██║██╔═══╝░██║░░██╗██║██║░░██║██║╚████║
+╚█████╔╝██║░░░░░╚█████╔╝██║╚█████╔╝██║░╚███║
+░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+
+
+██╗███╗░░██╗░█████╗░░█████╗░██████╗░██████╗░
+██║████╗░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║██╔██╗██║██║░░╚═╝██║░░██║██████╔╝██████╔╝
+██║██║╚████║██║░░██╗██║░░██║██╔══██╗██╔══██╗
+██║██║░╚███║╚█████╔╝╚█████╔╝██║░░██║██║░░██║
+╚═╝╚═╝░░╚══╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+
+███████╗░█████╗░████████╗░█████╗░
+██╔════╝██╔══██╗╚══██╔══╝██╔══██╗
+█████╗░░██║░░╚═╝░░░██║░░░███████║
+██╔══╝░░██║░░██╗░░░██║░░░██╔══██║
+███████╗╚█████╔╝░░░██║░░░██║░░██║
+╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝{color.fin}""")
+    time.sleep(4)
+    menu()
 def version():
  texto ="""
  |=======================================================|
@@ -78,4 +104,81 @@ def cabecera():
    ██║   ██║██╔═══╝    ██║   ██╔══╝  ██║╚██╔╝██║██║   ██║ ██╔██╗
    ██║   ██║██║        ██║   ███████╗██║ ╚═╝ ██║╚██████╔╝██╔╝╚██╗
    ╚═╝   ╚═╝╚═╝        ╚═╝   ╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝""")
-cabecera()
+def menu():
+    os.system("clear")
+    cabecera()
+    version()
+    print()
+    print(f"{color.verde}    QUE TE GUSTARIA APRENDER")
+    print(f"""{color.verde}
+[1]LISTAR CONTENIDO              LS
+[2]VER UBICACION                 PWD
+[3]LIMPIAR CONSOLA               CLEAR
+[4]PAQUETES                      PKG
+[5]HERRQMIENTAS                  PT
+[6]BORRA ARCHIVOS                RM                                                                       [7]ENTRAR EN UNA CARPETA         CD                                                                       [8]COPIAR UN ARCHIVO             CP
+[9]CREAR UN ARCHIVO              TOUCH
+[10]LEER UN ARCHIVO              CAT
+[11]SALIR                        EXIT
+[12]PERMISO CARPETAS TELEFONO    TERMUX-SETUP-STORAGE
+[13]CREAR CARPETAS               MKDIR
+{color.rojo}[0]SALIR{color.fin}
+""")                                                                                                          eleccion =input(f"{color.cyan}ELIJE UN NUMERO >>{color.fin} ")
+    if eleccion == "1" :
+        banner()
+    elif eleccion == "2" :
+      banner()
+    elif eleccion == "3" :
+     banner()
+    elif eleccion == "4" :
+     banner()
+    elif eleccion == "5" :
+     banner()
+    elif eleccion == "6" :
+     banner()
+    elif eleccion == "7" :
+     banner()
+    elif eleccion == "8" :
+     banner()
+    elif eleccion == "9" :
+     banner()
+    elif eleccion == "10" :
+     banner()
+    elif eleccion == "11" :
+     banner()
+    elif eleccion == "12" :
+     banner()
+    elif eleccion == "13" :
+     banner()
+    elif eleccion == "14" :
+     banner()
+    elif eleccion == "15" :
+     banner()
+    elif eleccion == "16" :
+     banner()
+    elif eleccion == "17" :
+     banner()
+    elif eleccion == "18" :
+     banner()
+    elif eleccion == "19" :
+     banner()
+    elif eleccion == "20" :
+     banner()
+    elif eleccion == "21" :
+     banner()
+    elif eleccion == "22" :
+     banner()
+    elif eleccion == "23" :
+     banner()
+    elif eleccion == "24" :
+     banner()
+    elif eleccion == "25" :
+     banner()
+    elif eleccion == "0" :
+     banner()
+     exit()
+    elif eleccion == "99" :
+     banner()
+    else:
+     incorrecto()
+menu()
